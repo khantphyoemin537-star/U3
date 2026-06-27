4883060a3b9a7a5e5c9c7eaa2e96989e29805558
khantphyoemin537-star <khantphyoemin537@gmail.com>
2026-06-27 14:07:09 +0630

    Update main.py

--- main.py
+++ main.py
@@ -14,7 +14,7 @@ from motor.motor_asyncio import AsyncIOMotorClient
 APP_ID = 39584681
 APP_HASH = 'c8c0685d6dd5b9e546093ea90d27733b'
 MONGO_URI = "mongodb+srv://kkt:h1BdaMt7nxW9jTXa@cluster0.kb5fzfl.mongodb.net/?appName=Cluster0&tlsAllowInvalidCertificates=true"
-BOT_TOKEN = '8575371720:AAHZJ-aP6mUsWIz4tl6k-S5Er23eXRIDYOs'
+BOT_TOKEN = '8738081667:AAHADgcDISntnOBwT3uj2yYw7n3XJUN2uZI'
 
 OWNER_ID = 7693106830
 SPECIFIC_GROUP = -1003848067679
